8140c674bf2859de13d90cc3aaaeb639ae216d1f
Farbod <farbodsaberi@gmail.com>
2024-10-14 21:43:47 +0330

    Create forLoopBounties.cs
    In Red dead redemption you have to calculate the amount of money gained from bounties.
    In this code I simulated how much money is gained after collecting 5 bounties.

---
+++ forLoopBounties.cs
@@ -0,0 +1,12 @@
+//for-loop bounties
+
+int totalEarnings = 0;
+
+
+for (int i = 0; i <= 5; i++)
+{
+    totalEarnings = totalEarnings + 50;
+
+    Console.WriteLine ("Collected bounty. Total earnings : " + totalEarnings);
+     
+}
